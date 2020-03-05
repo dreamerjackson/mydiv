c9a7ffa8112626ba6c85619d7fd98122dd49f850
jonson <zhuimengshaonian04@gmail.com>
2020-03-05 17:01:26 +0800

    feat: my div

--- div.go
+++ div.go
@@ -2,7 +2,7 @@ package mydiv
 import "github.com/pkg/errors"
 func Div(a int,b int) (int,error){
 	if b==0{
-		return 0,errors.Errorf("b can't = 0")
+		return 0,errors.Errorf("new error b can't = 0")
 	}
 	return a/b,nil
 }
