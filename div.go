c603516b855f982637536543cba8dbbe4316f791
jonson <zhuimengshaonian04@gmail.com>
2020-03-06 18:09:49 +0800

    feat: update

--- div.go
+++ div.go
@@ -3,7 +3,7 @@ import "github.com/pkg/errors"
 
 func Div(a int,b int) (int,error){
 	if b==0{
-		return 0,errors.Errorf("v1.0.3 b can't = 0")
+		return 0,errors.Errorf("v2.0.0 b can't = 0")
 	}
 	return a/b,nil
 }
